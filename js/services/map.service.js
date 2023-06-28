3a4e82c9246db3846ea1517d6524903067cfb11e
Guy Dahan <guy4191@gmail.com>
2023-06-28 15:43:51 +0300

    added API key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBhvpuMRtf5WHkvqgU3_jjbE3gC5La_mKI&callback=initMap' //DONE: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
